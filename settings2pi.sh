e9c3f3f7df37825f1fd178464109854af30a6d19
pimanDE <pimande@gmx.de>
2021-06-15 16:56:41 +0200

    Update settings2pi.sh
    Fixed

--- settings2pi.sh
+++ settings2pi.sh
@@ -423,7 +423,7 @@ sudo mv /etc/pihole/setupVars.conf /etc/pihole/setupVars.conf.orig		# Einstellun
 sudo wget -q https://raw.githubusercontent.com/pimanDE/settings2pi/master/Dateien/pihole/setupVars.conf -P /etc/pihole
 sudo chmod 777 /etc/pihole/setupVars.conf
 sudo rpl "local-ip" "$ipadresse" /etc/pihole/setupVars.conf > /dev/null 2>&1
-sudo rpl ' /24' '/24' /etc/pihole/pihole/setupVars.conf > /dev/null 2>&1
+sudo rpl ' /24' '/24' /etc/pihole/setupVars.conf > /dev/null 2>&1
 sudo chown root:root /etc/pihole/setupVars.conf
 sudo chmod 644 /etc/pihole/setupVars.conf
 
